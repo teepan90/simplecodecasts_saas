ce372c10b788602d467573dbdd5bca14df00a935
Teepan Nanthakumar <Teepan.nanthakumar@gmail.com>
2016-05-22 18:27:06 +0000

    Added validations and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,22 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new(contact_params)
         
+        if @contact.save
+            # A flash is part of rails to display messages, its a key value pair so basically a hash
+            flash[:success] = 'Message sent'
+            redirect_to new_contact_path
+        else
+            flash[:danger] = 'Error occured, message has not been sent'
+            redirect_to new_contact_path
+        end
     end
+    
+
+    private
+    def contact_params
+        params.require(:contact).permit(:name, :email, :comments)
+    end
+    
 end
