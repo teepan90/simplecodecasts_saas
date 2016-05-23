62a39fe8b02d38c246274d51a15565c0a3d4b910
Teepan Nanthakumar <Teepan.nanthakumar@gmail.com>
2016-05-23 22:44:02 +0000

    installed devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522205519) do
+ActiveRecord::Schema.define(version: 20160523223957) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
