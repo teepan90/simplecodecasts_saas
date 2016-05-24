cca84521a68f120b79192208ad6e4c50b4f34957
Teepan Nanthakumar <Teepan.nanthakumar@gmail.com>
2016-05-24 17:03:21 +0000

    Added plan variables and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
 
     def home
-    
-    
+        @basic_plan = Plan.find(1) #THis referes to our plans table and brings out the item with id 1
+        @pro_plan = Plan.find(3)
     end
+    
+    def about
+    end
+    
 end
 
