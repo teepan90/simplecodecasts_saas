161a799277906d93467aea6965e25d91d0672f7d
Teepan Nanthakumar <Teepan.nanthakumar@gmail.com>
2016-05-31 00:39:15 +0000

    Fixed typos in registrations_controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,18 +1,16 @@
-class Users::RegistrationsController < Devis::RegistrationsController
-
-    def create
-        
-        super do |resource|
-            if params[:plan]
-                resource.plan_id = paramsp[:plan] #User.first.plan_id = 1 or 3
-                if resource.plan_id == 2
-                    resource.save_with_payment
-                else
-                    resource.save_with_paymentend
-                end
-            end
-    
+class Users::RegistrationsController < Devise::RegistrationsController
+  
+  def create
+    super do |resource|
+      if params[:plan]
+        resource.plan_id = params[:plan]
+        if resource.plan_id == 3
+          resource.save_with_payment
+        else
+          resource.save
         end
+      end
     end
-    
+  end
+  
 end
